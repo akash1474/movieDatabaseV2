79570a3588c25bb9fc058eca8fcdcbf6f259a780
akash1474 <panditakash38@gmail.com>
2020-09-14 18:10:30 +0530

    bug

--- viewController.js
+++ viewController.js
@@ -2,16 +2,36 @@ const Axios =require('axios');
 const Movie=require('./test');
 exports.renderHomepage = async (req, res, next) => {
 	var data = await Axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.API_KEY}`);
+    const overall=[
+        {
+            _id:"movie",
+            totalCount:0,
+            totalSize:0,
+        },
+        {
+            _id:"tv",
+            totalCount:0,
+            totalSize:0,
+        }
+    ];
     const movies = await Movie.find();
-const overall=await Movie.aggregate([{
-                                            $group:{
+let info=await Movie.aggregate([{
+              $group:{
                                                     _id:'$category',
                                                     totalCount:{$sum:1},
                                                     totalSize:{$sum:'$size'}
                                                 }
-                                        }]);    res.status(200).render('index', {
+                                        }]);
+
+    console.log(info);
+    if(info.length===0){
+        console.log("RUnning");
+        info=overall;
+    }
+    console.log(info);
+    res.status(200).render('index', {
         data: movies,
         trending:data.data.results,
-        overallStats:overall,
+        overallStats:info,
     });
 };
